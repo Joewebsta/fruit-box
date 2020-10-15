5f5637120ebdb22b06c611fc6ff960e42d9eb787
Joe Webster <joseph.scott.webster@gmail.com>
2020-10-15 17:15:25 -0400

    Add exercise starter files

---
+++ main.js
@@ -0,0 +1,28 @@
+var fruits = [
+  {
+    name: 'Lemon',
+    img: 'https://bit.ly/2wQwmYG',
+    rotten: false,
+    id: 0
+  },
+  {
+    name: 'Lime',
+    img: 'https://bit.ly/344kBtA',
+    rotten: false,
+    id: 1
+  },
+  {
+    name: 'Apple',
+    img: 'https://bit.ly/2X1v3AJ',
+    rotten: true,
+    id: 2
+  }
+]
+
+// Query Selector
+var fruitBox = document.querySelector('.fruit-box')
+
+
+// render()
+// addFruit('Banana', 'https://bit.ly/3bNpVnT', false)
+// removeFruit(0) 
